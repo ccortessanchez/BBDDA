13344b8b6c9edf972277da00581d04dcc2027a29
ccortessanchez <ccortes.sanchez@gmail.com>
2015-10-12 03:27:08 +0200

    db file created

--- P1/main.sql
+++ P1/main.sql
@@ -8,43 +8,43 @@ DROP TABLE IF EXISTS format_per;
 
 
 
-CREATE TABLE menjar {
+CREATE TABLE menjar (
     nom VARCHAR(50) ,
     idMenjar INTEGER PRIMARY KEY NOT NULL ,
     descripcio VARCHAR(150)
 
-};
+);
 
-CREATE TABLE plat {
+CREATE TABLE plat (
     nom VARCHAR(50) ,
     idPlat INTEGER PRIMARY KEY NOT NULL ,
     descripcio VARCHAR(150)
-};
+);
 
-CREATE TABLE ingredient {
+CREATE TABLE ingredient (
     nom VARCHAR(50),
     idIngredient INTEGER ,
     refrigeracio BOOLEAN NOT NULL DEFAULT false ,
     familia INTEGER ,
     FOREIGN KEY (familia) REFERENCES familia(idFamilia)
-};
+);
 
-CREATE TABLE familia {
+CREATE TABLE familia (
     nom VARCHAR(50) ,
     idFamilia INTEGER PRIMARY KEY NOT NULL ,
     descripcio VARCHAR(150)
 
-};
+);
 
-CREATE TABLE xef {
+CREATE TABLE xef (
     nom VARCHAR(50)  ,
     idXef INTEGER PRIMARY KEY NOT NULL,
     estrella INTEGER
 
-};
+);
 
 
-CREATE TABLE recepta {
+CREATE TABLE recepta (
     nom VARCHAR(50) ,
     elaboracio VARCHAR(200) ,
     idRecepta INTEGER PRIMARY KEY NOT NULL ,
@@ -56,15 +56,15 @@ CREATE TABLE recepta {
     FOREIGN KEY(idX) REFERENCES xef(idXef) ,
     FOREIGN KEY(idP) REFERENCES plat(idPlat) ,
     FOREIGN KEY(idM) REFERENCES menjar(idMenjar) 
-};
+);
 
 
 
-CREATE TABLE format_per {
+CREATE TABLE format_per (
     idR INTEGER ,
     idIngr INTEGER ,
     FOREIGN KEY(idR) REFERENCES recepta(idRecepta), 
     FOREIGN KEY(idIngr) REFERENCES ingredient(idIngredient)
 
-};
+);
 
